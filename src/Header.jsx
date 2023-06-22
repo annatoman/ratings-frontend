30aa43a2194cd8aca18f789a2655f4d967ed3b18
Anna <annamtoman@gmail.com>
2023-06-22 11:26:26 -0500

    adjusts button and formats header

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,11 +4,11 @@ export function Header() {
   return (
     <header>
       <nav className="navbar bg-light">
-        <form className="container-fluid justify-right-content-start">
-        <img src={Logo} alt="Logo" width="200"/>
-          <button className="btn btn-sm btn-outline-secondary" type="button">Signup</button>
-          <button className="btn btn-sm btn-outline-secondary" type="button">Login</button>
-        </form>
+        <img src={Logo} alt="Logo" width="100"/>
+        <div style={{marginRight: '20px'}}>
+          <button className="btn" type="button">Login</button>
+          <button className="btn btn-secondary" type="button" style={{borderRadius: '16px'}}>Signup</button>
+        </div>
       </nav>
     </header>
   )
